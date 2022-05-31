32b6231001f75646f259d236acefb7944792e61d
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-05-31 16:37:05 -0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [details, setDetails] = useState()
+  const [budget, setBudget] = useState()
+
+  function newBudget(e){
+    e.preventDefault()
+    console.log('new budget')
+    setBudget(details)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form >
+        <h2>Budget</h2>
+        <div>
+          <input
+            placeholder='set your budget here'
+            onChange={(e) => setDetails(e.target.value)}
+          >
+          </input>
+          <input
+            type="submit"
+            onClick={(e) => newBudget(e)}
+          ></input>
+        </div>
+      </form>
+      <div>
+        <h2>Your Monthly Budget</h2>
+        <h3>${budget}</h3>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
+
+
+//input for establishing budget
+
+
+//create form for fixed costs
+  // form includes item, price
+
+
+//create form for additional expenses
+  // form item, price, type, importance 
+
+
+
